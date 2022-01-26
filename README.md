a7881a2883a3ea284c5a4e277a63c22d943ad8ca
Ellot Carson <carselli.c@gmail.com>
2022-01-25 19:53:42 -0600

    Initial code commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-mc-player-status-discord-messenger
+# Minecraft Player Status Discord Messenger
+
+## What this program does
+
+This program messeges a list of the current online players from a specified minecraft server to a Discord webhook.
+
+---
+## Required Packages
+- mcstatus
+- python-dotenv
+## How this program works
+This program works roughly as follows:
+1. Load .env variables
+2. Query the specified Minecraft server
+   1. Note: The Minecraft server must have `enable-query=true` set in it's `server.properties` file for this program to be able to query player names.
+3. Compares the returned names to the returned names from the previous query.
+4. If they are different, a message will be send to the specified Discord channel webhook.
+5. The latest player names are logged.
+---
+## Setup
+For this program to work, you will need to create a file names `.env` in the same directory containing the configuration info.
+The file may contain the following:
+|Variable Name | Required | Definition | Example Value |
+|--------------|------------|---------------|----------|
+|SERVER_ADDRESS | yes | Minecraft server IP Address or URL, including port number if different than 25565|51.178.221.10|
+|LAST_STATUS_FILE_NAME | yes | name of the file to store the player name list after query for future comparison | names.txt |
+|DISCORD_WEBHOOK_URL | yes | webhook for the discord channel the message will be sent to - more info [here](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks) | https://discord.com/api/webhooks/120643/acc8as80acdae |
+| DISCORD_USERNAME | yes | username shown in Discord channel when message is sent | mc-status-bot |
+| DISCORD_AVATAR_URL | no | URL of image to be shown in Discord when message is sent | www.picbin.com/apple.jpg |
+
+So an empty `.env` file would look like this:
+```
+SERVER_ADDRESS=
+LAST_STATUS_FILE_NAME=
+DISCORD_WEBHOOK_URL=
+DISCORD_USERNAME=
+DISCORD_AVATAR_URL=
+```
+---
+
+Shoutout to https://choosealicense.com/licenses/ for making open source software licences more approachable.
---
+++ check-players.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+import os
+import pytz
+from mcstatus import MinecraftServer
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+SERVER_ADDRESS = os.getenv('SERVER_ADDRESS')
+LAST_STATUS_FILE_NAME = os.getenv('LAST_STATUS_FILE_NAME')
+DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
+DISCORD_USERNAME = os.getenv('DISCORD_USERNAME')
+DISCORD_AVATAR_URL = os.getenv('DISCORD_AVATAR_URL')
+
+print("getting timezone")
+CST = pytz.timezone('America/Chicago')
+print("getting time")
+CURRENT_DATETIME = datetime.now(CST)
+
+
+def check_server():
+    print("checking server")
+    # Get the current server info
+    server = MinecraftServer.lookup(SERVER_ADDRESS)
+    # 'players' has attributes 'max', 'names' and 'online' (count)
+    players = server.query().players
+    # number of online players
+    online = players.online
+    # a list of the name strings
+    names = players.names
+
+    return names
+
+
+def load_last_status():
+    # loads last player name list
+    # if no file, or file is blank, returns []
+    names = []
+    #C:\Users\Elliot\Documents\Coding\mc-server-player-bot
+    
+    print("getting current working directory")
+    cwd_path = os.getcwd()
+    if os.path.isfile(f"{cwd_path}\{LAST_STATUS_FILE_NAME}"):
+        try:
+            last_status_file = open(LAST_STATUS_FILE_NAME, 'r')
+            names = last_status_file.read().split(',')
+            last_status_file.close()
+        except Exception as e:
+            print(e.with_traceback)
+    return names
+
+
+def update_last_status(names):
+    # updates the last status file w/ online and names list
+    last_status_file = open(LAST_STATUS_FILE_NAME, 'w')
+    last_status_file.write(', '.join(names))
+    last_status_file.close()
+
+
+def message_discord(names):
+    # sends a message to discord
+    message = "⛔ No current online players. ⛔"
+    if len(names) == 1:
+        message = f"🚶‍♂️ Current player: {', '.join(names)} 🚶‍♂️"
+    if len(names) > 1:
+        message = f"👯‍♂️ Current player: {', '.join(names)} 👯‍♂️"
+
+    data = {
+        "username": DISCORD_USERNAME,
+        "avatar_url": DISCORD_AVATAR_URL,
+        "content": message
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "charset": "utf-8"
+    }
+    response = requests.post(DISCORD_WEBHOOK_URL, headers=headers, json=data)
+
+
+def main():
+    names = check_server()
+    last_names = load_last_status()
+
+    names.sort()
+    last_names.sort()
+    if names == last_names:
+        # lists are the same
+        print(f"{CURRENT_DATETIME} - lists are the same. Current online players - {', '.join(names)}")
+    else:
+        # lists are not the same
+        print(f"{CURRENT_DATETIME} - the lists are different. Current online players - {', '.join(names)}")
+        update_last_status(names)
+        message_discord(names)
+    
+
+if (__name__ == "__main__"):
+    main()
